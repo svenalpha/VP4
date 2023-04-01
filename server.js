6514e4d97d6c64953db35b3b32d4f8f887460ac8
svenalpha <svalphaforsale@yahoo.com>
2023-04-01 13:47:02 +0700

    bad red, good yellow

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ import handlebars from "vite-plugin-handlebars";
 
 const app = express();              //  5173                   4173         
     ViteExpress.config({ mode: "production"}); //    test"}) // development,  production, test     
-  // ViteExpress.config({ mode: "test"});    
-// command in terminal:       NODE_ENV=production node server.ts/js  // for deployment version
+ //  ViteExpress.config({ mode: "test"});    
+// command in terminal:       NODE_ENV=production node server      .ts/js  // for deployment version
 import pkg from "vite-plugin-handlebars"; 
 
 
